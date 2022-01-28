1727f5a7c64f115d5702d2e4526d54487b4de7d7
MavFeuille <mavis.feuille@gmail.com>
2022-01-28 16:47:09 -0500

    Reset file to blank

--- Kata 6 - SmartParking.js
+++ Kata 6 - SmartParking.js
@@ -1,20 +1,3 @@
-const whereCanIPark = function (spots, vehicle) {
-
-  for (let y = 0; y < spots.length; y++) {
-    for (let x = 0; x < spots[y].length; x++) {
-
-      if (vehicle === 'regular' && spots[y][x] === 'R') {
-        return [x,y];
-      } else if (vehicle === 'small' && (spots[y][x] === 'R' || spots[y][x]  === 'S')) {
-          return [x,y];
-        } else if (vehicle === 'motorcycle' && (spots[y][x]  === 'R' || spots[y][x]  === 'S' || spots[y][x]  === 'M')) {
-          return [x,y];
-        }
-    }
-  }
-  return false;
-};
-
 
 
 console.log(whereCanIPark(
