1f8512ff996fee892df3d6aecec003ef0cabb628
MavFeuille <mavis.feuille@gmail.com>
2022-01-28 16:46:27 -0500

    Create another length solution with for loop

--- Kata 5 - Percent Encoded String .js
+++ Kata 5 - Percent Encoded String .js
@@ -1,6 +1,19 @@
 const urlEncode = (text) => {
   // Simple solution:
-  return text.trim().replaceAll(" ", "%20");
+  // return text.trim().replaceAll(" ", "%20");
+
+  // Solution without using string.prototype.replace:
+  text = text.trim();
+  let output = "";
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === " ") {
+      output += "%20";
+    } else {
+      output += text[i];
+    }
+  }
+  return output;
 }
 
 console.log(urlEncode("Lighthouse Labs"));
