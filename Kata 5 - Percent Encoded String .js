71210a911fe0346a67eaaeb6fab8e943e2ac8968
MavFeuille <mavis.feuille@gmail.com>
2022-01-28 16:30:26 -0500

    Reset file to blank

--- Kata 5 - Percent Encoded String .js
+++ Kata 5 - Percent Encoded String .js
@@ -1,16 +1,4 @@
-const urlEncode = function(text) {
-  text = text.trim()
-  let newString = "";
-  
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === " ") {
-      newString += "%20";
-    } else {
-      newString += text[i]
-    }
-  }
-  return newString;
-}
+
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
