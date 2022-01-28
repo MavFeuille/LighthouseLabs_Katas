93c6b0044962c29020bec5c8dc8138731b3806f4
MavFeuille <mavis.feuille@gmail.com>
2022-01-28 16:16:11 -0500

    Complete numberOfVowels function

--- Kata 3 - Vowels .js
+++ Kata 3 - Vowels .js
@@ -1,4 +1,13 @@
+const numberOfVowels = (data) => {
+  let count = 0;
 
+  for (let letter of data) {
+    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+      count += 1;
+    }
+  }
+  return count;
+}
 
 console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
