22ad17dea0f512b4463a3dcbf3f39148dc2fbce6
MavFeuille <mavis.feuille@gmail.com>
2022-01-29 16:41:05 -0500

    Remove original solution

--- Kata 7 - In the Air Tonight.js
+++ Kata 7 - In the Air Tonight.js
@@ -1,16 +1,4 @@
-const checkAir = function (samples, threshold) {
-  let dirtyCount = 0;  
 
-  for (let i = 0; i < samples.length; i++) {
-    if (samples[i] === 'dirty') {
-      dirtyCount ++
-    } 
-    if (dirtyCount / samples.length > threshold) {
-      return 'Polluted';
-    }
-  }
-  return 'Clean';
-}
 
 // checkAir(['clean', 'dirty], num of highest acceptable amount of DIRTY)
 
