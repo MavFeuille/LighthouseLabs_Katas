05385fe84a71d2d18ab2f7b9cbe952c3caadda4f
MavFeuille <mavis.feuille@gmail.com>
2022-01-31 00:50:26 -0500

    Set up nested for-loop

--- Kata 10 - Multiplication Table.js
+++ Kata 10 - Multiplication Table.js
@@ -1,11 +1,14 @@
 const multiplicationTable = (maxValue) => {
   let table = 0;
-  
-  for (let i = 0; i < maxValue.length; i++) {
 
+  for (let i = 0; i < maxValue.length; i++) {
+    for (let j = 0; j < maxValue.length; j++) {
+      
+    }
   }
+  
 }
 
 console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
-console.log(multiplicationTable(10));
+// console.log(multiplicationTable(10));
