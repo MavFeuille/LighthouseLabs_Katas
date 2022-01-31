cf3610a692d8e84dbd1c73e8328b255ccff57b1d
MavFeuille <mavis.feuille@gmail.com>
2022-01-31 00:16:15 -0500

    Remove previous solution

--- Kata 9 - Case Maker.js
+++ Kata 9 - Case Maker.js
@@ -1,16 +1,4 @@
-const camelCase = function(input) {
-  let finalText = "";
-  for (let i = 0 ; i < input.length; i++) {
-    if (input[i] !== " ") {
-      finalText += input[i];
-    } else {
-      let capLetter = input[i+1].toUpperCase();
-      finalText += capLetter;
-      i++;
-    }
-  }
-  return finalText;
-}
+
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
