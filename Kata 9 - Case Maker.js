5ff3f6e5dee34c7475b5e09409ab2c47b77f0af5
MavFeuille <mavis.feuille@gmail.com>
2022-01-31 00:33:09 -0500

    Add notes at Line 10

--- Kata 9 - Case Maker.js
+++ Kata 9 - Case Maker.js
@@ -1,5 +1,16 @@
 const camelCase = (input) => {
-  
+  let output = "";
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== " ") {
+      output += input[i];
+    } else {
+      let capLetter = input[i + 1].toUpperCase();
+      output += capLetter;
+      i++;  //when it loop to space, it will return undefined and increment to the next character
+    }
+  }
+  return output;
 }
 
 console.log(camelCase("this is a string"));
