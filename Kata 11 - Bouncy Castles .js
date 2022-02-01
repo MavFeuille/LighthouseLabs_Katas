a7e81a902f48169008f968524d085f1d5ba0fc9c
MavFeuille <mavis.feuille@gmail.com>
2022-02-01 15:57:52 -0500

    Complete sphereVolume function

--- Kata 11 - Bouncy Castles .js
+++ Kata 11 - Bouncy Castles .js
@@ -3,7 +3,9 @@ const PI = 3.14159;
 
 
 const sphereVolume = (radius) => {
-
+  // V = 4/3 PI r^3
+  return 4 / 3 * PI * Math.pow(radius, 3);
+  
 }
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
