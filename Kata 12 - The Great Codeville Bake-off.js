335c7c30cf2d17707780d5cfc1378b9d3f08d632
MavFeuille <mavis.feuille@gmail.com>
2022-02-01 16:39:02 -0500

    Update function to be an arrow function

--- Kata 12 - The Great Codeville Bake-off.js
+++ Kata 12 - The Great Codeville Bake-off.js
@@ -1,5 +1,5 @@
 // Helper function  to check if the given bakery possesses any of ingredients from that recipe
-const ingredientCheck = (bakery, ingredients) {
+const ingredientCheck = (bakery, ingredients) => {
 
 }
 
