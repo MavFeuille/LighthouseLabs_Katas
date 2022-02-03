a9a7977ea7d8731215da94a4ce99a283f623d8e1
MavFeuille <mavis.feuille@gmail.com>
2022-02-02 22:38:49 -0500

    Create function to check ingredient

--- Kata 12 - The Great Codeville Bake-off.js
+++ Kata 12 - The Great Codeville Bake-off.js
@@ -1,8 +1,3 @@
-// Helper function  to check if the given bakery possesses any of ingredients from that recipe
-const ingredientCheck = (bakery, ingredients) => {
-
-}
-
 let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
 let bakeryB = ['milk', 'butter', 'cream cheese'];
 let recipes = [
@@ -20,4 +15,20 @@ let recipes = [
     }
 ];
 
-console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+// console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+// Helper function  to check if the given bakery possesses any of ingredients from that recipe
+const ingredientCheck = (bakery, ingredients) => {
+  for (const bakeryIngredient of bakery) {
+    // console.log("ingredients in bakery: ", bakeryIngredient)
+    for (let i = 0; i < recipes.length; i ++) {
+      // console.log("ingredients in recipes: ", recipes[i].ingredients);
+      // console.log("bakeryIngredient: ", bakeryIngredient);
+      if (recipes[i].ingredients.includes(bakeryIngredient)) {
+        console.log("bakeryIngredient: ", bakeryIngredient, "recipes: ", recipes[i]);
+      }
+    }
+  }
+}
+
+console.log(ingredientCheck(bakeryA, recipes))
