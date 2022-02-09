2602a063b3616dadd2ed8e8fbb4d0afe5c2b6410
MavFeuille <mavis.feuille@gmail.com>
2022-02-08 22:43:39 -0500

    Add conditional statements for day

--- Kata 13 - Talking Calendar.js
+++ Kata 13 - Talking Calendar.js
@@ -5,6 +5,16 @@ const talkingCalendar = (date) => {
   let day = Number(dateArr[2]);
 
   const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  if (day === 1 || day === 21 || day === 31) {
+    day += "st";
+  } else if (day === 2 || day === 22) {
+    day += "nd";
+  } else if (day === 3 || day === 23) {
+    day += "rd";
+  } else {
+    day += "th";
+  }
 }
 
 console.log(talkingCalendar("2017/12/02"));
