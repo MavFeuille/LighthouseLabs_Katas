450ffec0a946f9fe678a0ffb3b48d62a700a7d7c
MavFeuille <mavis.feuille@gmail.com>
2022-02-09 16:29:25 -0500

    Update placeholder for output

--- Kata 14 - Change Calculator.js
+++ Kata 14 - Change Calculator.js
@@ -1,11 +1,29 @@
 const calculateChange = (total, cash) => {
-
+  /* conversion rate
+  penny = 1
+  nickel = 5 * penny;
+  dime = 10 * penny;
+  quarter = 25 * penny;
+  oneDollar = 100 * penny;
+  twoDollars = 200 * penny;
+  fiveDollars = 500 * penny;
+  tenDollars = 1000 * penny;
+  twentyDollars = 2000 * p
+  */
+ 
   // change
   let change = cash - total;
+
   // placeholder for output
-  let output = {};
+  let output = {}
+  };
 
-
+  while (change > 0) {
+    if (change >= 2000) {
+      output.twentyDollar += 1;
+      change -= 1000;
+    } else if ()
+  }
 }
 
 console.log(calculateChange(1787, 2000));
