6b22e48123c4bdb065b9d32e7469654775be9a66
MavFeuille <mavis.feuille@gmail.com>
2022-02-08 22:41:26 -0500

    Set placeholder for month names

--- Kata 13 - Talking Calendar.js
+++ Kata 13 - Talking Calendar.js
@@ -4,7 +4,7 @@ const talkingCalendar = (date) => {
   let month = Number(dateArr[1]);
   let day = Number(dateArr[2]);
 
-  
+  const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 }
 
 console.log(talkingCalendar("2017/12/02"));
