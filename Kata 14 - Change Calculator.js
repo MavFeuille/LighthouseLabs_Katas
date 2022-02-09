fd0c699913615ef5bcd0ba1bccf32e2736b053e5
MavFeuille <mavis.feuille@gmail.com>
2022-02-09 15:55:57 -0500

    Set placeholders for coins conversion rates

--- Kata 14 - Change Calculator.js
+++ Kata 14 - Change Calculator.js
@@ -1,5 +1,15 @@
 const calculateChange = (total, cash) => {
-  
+  // conversion rate
+  const penny = 1
+  const nickel = 5 * penny;
+  const dime = 10 * penny;
+  const quarter = 25 * penny;
+  const oneDollar = 100 * penny;
+  const twoDollars = 200 * penny;
+  const fiveDollars = 500 * penny;
+  const tenDollars = 1000 * penny;
+  const twentyDollars = 2000 * penny;
+  // change
 }
 
 console.log(calculateChange(1787, 2000));
