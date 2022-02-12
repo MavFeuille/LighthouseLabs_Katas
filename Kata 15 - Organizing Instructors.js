02d1523d288be804c495c02dba1c29a064e62258
MavFeuille <mavis.feuille@gmail.com>
2022-02-11 23:21:47 -0500

    Add another test case

--- Kata 15 - Organizing Instructors.js
+++ Kata 15 - Organizing Instructors.js
@@ -24,6 +24,13 @@ const organizeInstructors = (instructors) => {
 }
 
 console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+
+console.log(organizeInstructors([
   {name: "Brendan", course: "Blockchain"},
   {name: "David", course: "Web"},
   {name: "Martha", course: "iOS"},
