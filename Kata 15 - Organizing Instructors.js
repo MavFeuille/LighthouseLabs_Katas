b038fefbe159642b028f34d74647297f6420d994
MavFeuille <mavis.feuille@gmail.com>
2022-02-11 23:00:07 -0500

    Set up for loop to push course name into an array

--- Kata 15 - Organizing Instructors.js
+++ Kata 15 - Organizing Instructors.js
@@ -1,11 +1,16 @@
 const organizeInstructors = (instructors) => {
   let output = {};
   let instructorsArray = [];
+  let courseNames = [];
 
+  // Check if the course name has appeared in courseNames array, if not, push it to courseNames array
   for (const obj of instructors){
-    
+    // let course = obj.course;
+    if (courseNames.indexOf(obj.course) === -1){
+      courseNames.push(obj.course);
+    }
+    // console.log("courseNames: ", courseNames);
   }
- 
 }
 
 console.log(organizeInstructors([
