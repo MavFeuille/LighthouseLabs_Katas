ee6fac4fae959ad40695e1b8b58a20d395286480
MavFeuille <mavis.feuille@gmail.com>
2022-02-11 23:21:36 -0500

    Complete organizeInstructors

--- Kata 15 - Organizing Instructors.js
+++ Kata 15 - Organizing Instructors.js
@@ -3,11 +3,24 @@ const organizeInstructors = (instructors) => {
   let courseNames = [];
 
   for (const obj of instructors){
+    // If there is no such course name in courseNames array, push this course name into courseNames
     if (courseNames.indexOf(obj.course) === -1){
       courseNames.push(obj.course);
     }
-    console.log("courseNames: ", courseNames)
   }
+
+  courseNames.forEach((course) => {
+    // Set each course name be the key of output and assign undefined array as its value, i.e { Blockchain: [] }
+    output[course] = [];
+
+    // Loop through instructors array, if the course name from courseNames = instructors.course, push the instructor's name into courseNames' undefined array
+    for (const obj of instructors){
+      if (course === obj.course){
+        output[course].push(obj.name);
+      }
+    }
+  })
+  return output;
 }
 
 console.log(organizeInstructors([
