63401de1838f8bc2207c2ad408d3ad5584e6f6f0
MavFeuille <mavis.feuille@gmail.com>
2022-02-12 21:28:54 -0500

    Input precedence instructions from kata

--- Kata 16 - Case Maker II.js
+++ Kata 16 - Case Maker II.js
@@ -1,3 +1,12 @@
-const makeCase = function(input, cases){
+/*
+Precedence of each of the casing styles are as follows, values higher in the list should be processed first:
+1. camel, pascal, snake, kebab, title
+2. vowel, consonant
+3. upper, lower 
+*/
 
+const makeCase = function(input, cases){
+  let output = "";
+
+  if (cases === "")
 }
