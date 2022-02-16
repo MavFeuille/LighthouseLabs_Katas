9eea370556d6081fb450cf4beef9fb97e6cbb0d7
MavFeuille <mavis.feuille@gmail.com>
2022-02-16 00:05:38 -0500

    Add test case for "consonant"

--- Kata 16 - Case Maker II.js
+++ Kata 16 - Case Maker II.js
@@ -86,3 +86,4 @@ const makeCase = (input, cases) => {
 // console.log(makeCase("this is a string", "kebab"));
 // console.log(makeCase("this is a string", "title"));
 // console.log(makeCase("this is a string", "vowel"));
+console.log(makeCase("this is a string", "consonant"));
