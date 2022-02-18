38460e663b305e01b170da3da197e55976169299
MavFeuille <mavis.feuille@gmail.com>
2022-02-18 17:10:33 -0500

    Add test case for 2 key value pairs

--- Kata 17 - JS Object From URL Encoded String.js
+++ Kata 17 - JS Object From URL Encoded String.js
@@ -1,3 +1,8 @@
 const urlDecode = (text) => {
+
+  let removeAmpersandText = text.replace("&", ", ");
+  console.log("🚀 ~ file: Kata 17 - JS Object From URL Encoded String.js ~ line 4 ~ urlDecode ~ removeAmpersandText", removeAmpersandText)
   
 }
+
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
