bf1eb01f00f0cbe3d3c8882939853cdd2eb4caab
MavFeuille <mavis.feuille@gmail.com>
2022-02-18 16:48:36 -0500

    Remove "lower" case example

--- Kata 16 - Case Maker II.js
+++ Kata 16 - Case Maker II.js
@@ -115,5 +115,4 @@ console.log(makeCase("this is a string", "kebab"));
 console.log(makeCase("this is a string", "title"));
 console.log(makeCase("this is a string", "vowel"));
 console.log(makeCase("this is a string", "consonant"));
-console.log(makeCase("THIS IS A STRING", "lower"));
 console.log(makeCase("this is a string", ["upper", "snake"]));
