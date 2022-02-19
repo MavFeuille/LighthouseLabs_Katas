43e9a14ab0ef1cded5866d41955858041c137329
MavFeuille <mavis.feuille@gmail.com>
2022-02-19 16:34:04 -0500

    rename key as a variable in for-loop

--- Kata 17 - JS Object From URL Encoded String.js
+++ Kata 17 - JS Object From URL Encoded String.js
@@ -10,14 +10,11 @@ const urlDecode = (text) => {
   console.log("🚀 ~ file: Kata 17 - JS Object From URL Encoded String.js ~ line 9 ~ urlDecode ~ newText", newText)
   
   for (const item of newText){
- 
     let key = item.slice(0, item.indexOf(":"));
     let value = item.slice(item.indexOf(":"));
-    // console.log("🚀 ~ file: Kata 17 - JS Object From URL Encoded String.js ~ line 16 ~ urlDecode ~ value", value)
-    // console.log("🚀 ~ file: Kata 17 - JS Object From URL Encoded String.js ~ line 14 ~ urlDecode ~ key:", key)
-    // console.log("🚀 ~ file: Kata 17 - JS Object From URL Encoded String.js ~ line 14 ~ urlDecode ~ newItem", newItem)
-
+    output = {[key]: [value]}
   }
+  return output;
 }
 
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
