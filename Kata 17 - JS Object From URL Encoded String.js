504262d3f2f4644f3cfe3d0b9805ff3a7091effb
MavFeuille <mavis.feuille@gmail.com>
2022-02-19 22:47:10 -0500

    Console log various test cases

--- Kata 17 - JS Object From URL Encoded String.js
+++ Kata 17 - JS Object From URL Encoded String.js
@@ -20,4 +20,7 @@ const urlDecode = (text) => {
   return output;
 }
 
+console.log(urlDecode("duck=rubber"));
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
