2355b70fa37d265164024e0d6ed463965cb6b60e
MavFeuille <mavis.feuille@gmail.com>
2022-02-19 23:04:08 -0500

    Add 1st test case

--- Kata 18 - Square Code.js
+++ Kata 18 - Square Code.js
@@ -1,3 +1,11 @@
 const squareCode = (message) => {
+  let output = "";
   
+  // Remove spaces from original message
+  message = message.replace(/\s/g, ""); 
+
+  // Find out number of columes through the length of message
+  let columns = Math.ceil(Math.sqrt(message.length));
 }
+
+console.log(squareCode("chill out"));
