6841fdc1591b154783c3bc971cf338611632c75e
MavFeuille <mavis.feuille@gmail.com>
2022-02-19 23:20:31 -0500

    Set up for-loop to write the message into a rectangle

--- Kata 18 - Square Code.js
+++ Kata 18 - Square Code.js
@@ -2,10 +2,16 @@ const squareCode = (message) => {
   let output = "";
   
   // Remove spaces from original message
-  message = message.replace(/\s/g, ""); 
+  message = message.replace(/\s/g, "");
 
   // Find out number of columes through the length of message
   let columns = Math.ceil(Math.sqrt(message.length));
-}
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = i; j < message.length; j++) {
+      console.log("j: ", columns[i][j]);
+    }
+  }
+};
 
 console.log(squareCode("chill out"));
