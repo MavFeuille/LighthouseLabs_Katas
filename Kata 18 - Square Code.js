b2d0752796e95b6c1bd7b7b21e3555c672ac46c3
MavFeuille <mavis.feuille@gmail.com>
2022-02-20 23:48:23 -0500

    Remove test cases

--- Kata 18 - Square Code.js
+++ Kata 18 - Square Code.js
@@ -25,5 +25,3 @@ const squareCode = (message) => {
 
 console.log(squareCode("chill out"));
 console.log(squareCode("feed the dog"));
-console.log(squareCode("have a nice day"));
-console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
