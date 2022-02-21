82622190db3140f996c3cfa3105ccd36c6bbd702
MavFeuille <mavis.feuille@gmail.com>
2022-02-20 23:32:56 -0500

    Update nested for loop to print message in square

--- Kata 18 - Square Code.js
+++ Kata 18 - Square Code.js
@@ -8,9 +8,13 @@ const squareCode = (message) => {
   let columns = Math.ceil(Math.sqrt(message.length));
   console.log("🚀 ~ file: Kata 18 - Square Code.js ~ line 9 ~ squareCode ~ columns", columns)
 
+  // Perform for loop whenever i < 3 (columns)
   for (let i = 0; i < columns; i++) {
     // console.log("Message in i-loop: ", message);
-    
+    console.log("columns[i] :", i);
+    for (let j = i; j < message.length; j += columns) {
+      console.log("message[j] :", message[j]);
+    }
   }
 };
 
