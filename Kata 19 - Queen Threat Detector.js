79bebec694a184d765666930a90b93293636e312
MavFeuille <mavis.feuille@gmail.com>
2022-02-21 22:25:55 -0500

    Set variable for boardColumns (board width)

--- Kata 19 - Queen Threat Detector.js
+++ Kata 19 - Queen Threat Detector.js
@@ -1,5 +1,8 @@
 let whiteQueen = [0, 0];
 let blackQueen = [5, 7];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
+let boardColumns = 8;
+
+
 console.log(generatedBoard);
 console.log(queenThreat(generatedBoard));
