1608dfc5b42111665ef05149a1cdba07db2489ea
MavFeuille <mavis.feuille@gmail.com>
2022-02-21 22:26:48 -0500

    Set placeholder for output

--- Kata 19 - Queen Threat Detector.js
+++ Kata 19 - Queen Threat Detector.js
@@ -2,7 +2,11 @@ let whiteQueen = [0, 0];
 let blackQueen = [5, 7];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
 let boardColumns = 8;
+let output = [];
 
+const generateBoard = (whiteQueen, blackQueen) => {
+ 
+}
 
 console.log(generatedBoard);
 console.log(queenThreat(generatedBoard));
