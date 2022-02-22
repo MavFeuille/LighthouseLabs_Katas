938bf630d180c9b0f75f895d8add6db9efb33f7f
MavFeuille <mavis.feuille@gmail.com>
2022-02-21 22:28:43 -0500

    Re-order output, boardColumns and gernerateBoard function to the top

--- Kata 19 - Queen Threat Detector.js
+++ Kata 19 - Queen Threat Detector.js
@@ -1,12 +1,16 @@
+let output = [];
+let boardColumns = 8;
+
+const generateBoard = (whiteQueen, blackQueen) => {
+  for (let i = 0; i < boardColumns; i++) {
+    console.log(output[i]);
+  }
+}
+
+
 let whiteQueen = [0, 0];
 let blackQueen = [5, 7];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
-let boardColumns = 8;
-let output = [];
-
-const generateBoard = (whiteQueen, blackQueen) => {
- 
-}
 
 console.log(generatedBoard);
 console.log(queenThreat(generatedBoard));
