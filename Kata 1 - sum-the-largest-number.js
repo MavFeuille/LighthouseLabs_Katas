212a6a8b79730d6006cf539f71ce8f6783b7aafc
MavFeuille <mavis.feuille@gmail.com>
2022-02-22 23:19:17 -0500

    Input the given instructions

--- Kata 1 - sum-the-largest-number.js
+++ Kata 1 - sum-the-largest-number.js
@@ -1,3 +1,5 @@
+//Given an array of 2 or more numbers. We will then have to find the two largest numbers in that array, and sum them together.
+
 const sumLargestNumbers = data => {
   let firstLargest = 0;
   let secondLargest = 0;
