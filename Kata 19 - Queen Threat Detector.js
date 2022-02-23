ad603f256bc8ddf318ae37d76ed1dbebafe2f1b3
MavFeuille <mavis.feuille@gmail.com>
2022-02-22 22:51:33 -0500

    Update nested for-loop to create 8 x 8 chessboard

--- Kata 19 - Queen Threat Detector.js
+++ Kata 19 - Queen Threat Detector.js
@@ -2,10 +2,12 @@ let output = [];
 let boardColumns = 8;
 
 const generateBoard = (whiteQueen, blackQueen) => {
+  // Create 8 x 8 chessboard 
   for (let i = 0; i < boardColumns; i++) {
     output[i] = [];
     for (let j = 0; j < boardColumns; j++) {
-      
+      // 0 in array represents an unoccupied square
+      output[i].push(0);
     }
   }
 }
