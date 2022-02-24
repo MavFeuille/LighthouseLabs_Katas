eadeb8193bf2c11da50aa433159c489c8049bc27
MavFeuille <mavis.feuille@gmail.com>
2022-02-23 22:52:52 -0500

    Move placeholders for queens X and Y axis to outer scope

--- Kata 19 - Queen Threat Detector.js
+++ Kata 19 - Queen Threat Detector.js
@@ -1,14 +1,16 @@
+let whiteQueen = [0, 0];
+let blackQueen = [5, 7];
 let output = [];
 let boardColumns = 8;
 
+// Placeholders for whiteQueen and blackQueen X and Y axis
+let whiteX = whiteQueen[0];
+let whiteY = whiteQueen[1];
+let blackX = blackQueen[0];
+let blackY = blackQueen[1];
+
 
 const generateBoard = (whiteQueen, blackQueen) => {
-  // Placeholders for whiteQueen and blackQueen X and Y axis
-  let whiteX = whiteQueen[0];
-  let whiteY = whiteQueen[1];
-  let blackX = blackQueen[0];
-  let blackY = blackQueen[1];
-
   // Create 8 x 8 chessboard 
   for (let i = 0; i < boardColumns; i++) {
     output[i] = [];
@@ -24,12 +26,11 @@ const generateBoard = (whiteQueen, blackQueen) => {
 };
 
 // Check if 2 queens are positioned to attack each other
-const queenThreat = (generateBoard) => {
-
+const queenThreat = (generatedBoard) => {
+ 
 };
 
-let whiteQueen = [0, 0];
-let blackQueen = [5, 7];
+
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
 
 console.log(generatedBoard);
