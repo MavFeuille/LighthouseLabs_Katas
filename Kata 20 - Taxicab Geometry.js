626d19cc60b50efae05cc02ee03a06bc714a24dc
MavFeuille <mavis.feuille@gmail.com>
2022-02-24 23:14:42 -0500

    Update heading for first direction from originate

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -2,7 +2,7 @@ const blocksAway = (directions) => {
   let blocksAway = {east: 0, north: 0};
   let inDirection = "";
 
-  // Set the first direction from originate
+  // Set the first direction from originate. Clues come from the first 2 indices of (directions)
   if (directions[0] === "right") {
     blocksAway.east += directions[1];
     inDirection = "east";
@@ -10,6 +10,8 @@ const blocksAway = (directions) => {
     blocksAway.north += directions[1];
     inDirection = "north";
   }
+
+
 }
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
