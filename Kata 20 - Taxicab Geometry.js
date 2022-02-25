7ce170f3c125b58c6d9645d76fccfc29f6a63039
MavFeuille <mavis.feuille@gmail.com>
2022-02-24 23:13:08 -0500

    Set up conditional statements to find the first direction from originate

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -1,8 +1,15 @@
 const blocksAway = (directions) => {
   let blocksAway = {east: 0, north: 0};
-  let currentDirection = "";
+  let inDirection = "";
 
-  
+  // Set the first direction from originate
+  if (directions[0] === "right") {
+    blocksAway.east += directions[1];
+    inDirection = "east";
+  } else {
+    blocksAway.north += directions[1];
+    inDirection = "north";
+  }
 }
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
