4b1030fe763ffa0c48c5a7f6ca3c19f2b0046ff6
MavFeuille <mavis.feuille@gmail.com>
2022-02-24 23:21:57 -0500

    Update variable name to currentDirection

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -1,17 +1,22 @@
 const blocksAway = (directions) => {
   let blocksAway = {east: 0, north: 0};
-  let inDirection = "";
+  let currentDirection = "";
 
   // Set the first direction from originate. Clues come from the first 2 indices of (directions)
   if (directions[0] === "right") {
     blocksAway.east += directions[1];
-    inDirection = "east";
+    currentDirection = "east";
   } else {
     blocksAway.north += directions[1];
-    inDirection = "north";
+    currentDirection = "north";
   }
+  console.log("Current direction after the first step:", currentDirection);
+  console.log("blocksAway after the first step:", blocksAway);
 
-
+  // Loop through (directions) from 2nd index onwards to find the coming directions
+  // for (let i = 2; i < directions.length; i += 2) {
+  //   if (directions[i] === "right") {}
+  // }
 }
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
