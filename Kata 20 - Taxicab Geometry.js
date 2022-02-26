336dcc8a58b40f502c966606157263c5afce666a
MavFeuille <mavis.feuille@gmail.com>
2022-02-25 22:22:02 -0500

    Set up conditional statement to find 1st position after 1st step

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -1,9 +1,15 @@
 const blocksAway = (directions) => {
   let finalPosition = {east: 0, north: 0};
-  let faceDirection = "";
+  let facing = "";
 
-  // Find position of the 1st step
-  
+  // Find position of the 1st step. (Base on the instructions, either left or right will be given)
+  if (directions[0] === "right") {
+    finalPosition.east += 1;
+    facing = "east";
+  } else {
+    finalPosition.north += 1;
+    facing = "north";
+  }
   // Find position AFTER the 1st step
 
 
