baaf37ace27e443e74e0d2ab6b20e9059683ca76
MavFeuille <mavis.feuille@gmail.com>
2022-02-25 22:16:47 -0500

    Update parameter

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -1,6 +1,11 @@
-const blocksAway = (direction) => {
+const blocksAway = (directions) => {
   let finalPosition = {east: 0, north: 0};
+  let faceDirection = "";
 
-  
+  // Find position of the 1st step
+ 
+  // Find position AFTER the 1st step
+
+
 };
 
