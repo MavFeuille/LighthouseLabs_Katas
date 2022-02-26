2aa493d7df9d92375d53b29116506dd9de22efa2
MavFeuille <mavis.feuille@gmail.com>
2022-02-25 22:41:36 -0500

    Set up for loop to get each direction from parameter

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -10,7 +10,28 @@ const blocksAway = (directions) => {
     finalPosition.north += 1;
     facing = "north";
   }
-  // Find position AFTER the 1st step
+  console.log("Position of 1st step at Line 13: ", finalPosition);
+
+  // Find position AFTER the 1st step, i.e. after 2nd index in the parameter
+  for (let i = 2; i < directions.length; i += 2) {
+    if (directions[i] === "left") {
+      // Check current facing direction before moving "left" then assign new direction after moved to "left"
+      if (facing === "east") {
+        facing = "north";
+      } 
+      if (facing === "south") {
+        facing = "east";
+      } 
+      if (facing === "west") {
+        facing = "south";
+      } 
+      if (facing === "north") {
+        facing === "west";
+      }
+    } 
+    console.log("Position after direction[i]: ", finalPosition);
+
+  }
 
 
 };
