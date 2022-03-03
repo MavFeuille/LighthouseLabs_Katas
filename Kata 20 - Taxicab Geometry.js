3cc262e932c8b0d77557dccafe942e1e9de3cb9d
MavFeuille <mavis.feuille@gmail.com>
2022-03-02 22:37:56 -0500

    Update comment

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -14,7 +14,11 @@ const blocksAway = (directions) => {
   console.log("Currently facing direction after 1st move at Line 14: ", currentFacing);
   console.log("1st move finalPosition at Line 15: ", finalPosition);
 
-  // Loop through directions to find out the next directions base on the first move
+  // Loop through directions to find out the next directions base on the first move.
+  // Loop directions from 3rd index and increment by 2 since the first 2 indices are already checked above.
+  for (let i = 2; i < directions.length; i += 2) {
+    
+  }
 }
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
