b5670dd20450733744cb6c76d5d9dac8e71a84d9
MavFeuille <mavis.feuille@gmail.com>
2022-03-02 22:31:07 -0500

    Add pseudo codes

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -1,6 +1,9 @@
 const blocksAway = (directions) => {
   let finalPosition = {east: 0, north: 0};
   let currentlyFacing = "";
+
+  // Find out the 1st move of direction and blocks from the first two indices from directions
+  // Loop through directions to find out the next directions base on the first move
 }
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
