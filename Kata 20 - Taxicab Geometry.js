ef15e9ef550c3743443363b940c9353954c0e423
MavFeuille <mavis.feuille@gmail.com>
2022-03-02 22:33:45 -0500

    Rename variable to currentFacing

--- Kata 20 - Taxicab Geometry.js
+++ Kata 20 - Taxicab Geometry.js
@@ -1,10 +1,16 @@
 const blocksAway = (directions) => {
   let finalPosition = {east: 0, north: 0};
-  let currentlyFacing = "";
+  let currentFacing = "";
 
   // Find out the 1st move of direction and blocks from the first two indices from directions.
   // Base on the instructions, it will either start from left or right
-  if (directions[0] === "right")
+  if (directions[0] === "right") {
+    finalPosition.east += directions[1];
+    currentFacing = "east";
+  } else {
+    finalPosition.north += directions[1];
+    currentFacing = "north";
+  }
   // Loop through directions to find out the next directions base on the first move
 }
 
