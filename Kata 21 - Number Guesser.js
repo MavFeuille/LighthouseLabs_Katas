dd37524ce044f40590084f0ece46dfa2c931bb4b
MavFeuille <mavis.feuille@gmail.com>
2022-03-03 23:23:43 -0500

    Generate random number

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -1,3 +1,6 @@
 const prompt = require('prompt-sync')();
 
-
+// Generate random number
+const maxNum = 50;
+const randomNumber = (Math.ceil(Math.random() * maxNum));
+// console.log("randomNumber = ",randomNumber)
