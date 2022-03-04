20a523ffb914d412d8a4264480d0fb8f38715df8
MavFeuille <mavis.feuille@gmail.com>
2022-03-03 23:25:08 -0500

    Change randomNumber from const to let

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -2,5 +2,5 @@ const prompt = require('prompt-sync')();
 
 // Generate random number
 const maxNum = 50;
-const randomNumber = (Math.ceil(Math.random() * maxNum));
+let randomNumber = (Math.ceil(Math.random() * maxNum));
 // console.log("randomNumber = ",randomNumber)
