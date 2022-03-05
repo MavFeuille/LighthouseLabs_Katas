487a92bfaee017b226b97dc1cb8d8e9c08d8830d
MavFeuille <mavis.feuille@gmail.com>
2022-03-04 21:52:27 -0500

    Add if else statement to compare prompt vs randomNumber

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -3,10 +3,18 @@ const prompt = require('prompt-sync')();
 // Generate random number
 const maxNum = 50;
 const randomNumber = (Math.ceil(Math.random() * maxNum));
-// console.log("randomNumber = ",randomNumber)
+console.log("randomNumber = ",randomNumber)
 
 const guessNumber = (randomNumber) => {
   let attempt = 0;
   let input = prompt("Guess a number: ");
+  if (prompt < randomNumber) {
+    console.log("You answerd: " + input);
+    console.log("Tow low!");
+  } else if (prompt > randomNumber) {
+    console.log("You answerd: " + input);
+    console.log("Tow high!");
+  }
 }
 
+guessNumber(randomNumber);
