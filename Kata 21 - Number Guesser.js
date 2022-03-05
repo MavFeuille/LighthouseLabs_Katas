484c378a82e307138ce8e379d7b0184e226f9765
MavFeuille <mavis.feuille@gmail.com>
2022-03-04 22:05:58 -0500

    Change typeof input to be number

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -7,19 +7,20 @@ console.log("randomNumber = ",randomNumber)
 
 const guessNumber = (randomNumber) => {
   let attempt = 0;
-  let input = prompt("Guess a number: ");
-  if (prompt < randomNumber) {
+  let input = Number(prompt("Guess a number: "));
+  
+  if (input < randomNumber) {
     console.log("You answerd: " + input);
     console.log("Tow low!");
     attempt += 1;
-  } else if (prompt > randomNumber) {
+  } else if (input > randomNumber) {
     console.log("You answerd: " + input);
     console.log("Tow high!");
     attempt += 1;
-  } else if (isNaN(prompt)) {
+  } else if (typeof input !== Number) {
     console.log("You answerd: " + input);
     console.log("Not a number, try again!")
-  } else if (prompt === randomNumber) {
+  } else if (input == randomNumber) {
     attempt += 1;
     console.log("You answerd: " + input);
     console.log(`You got it! You took ${attempt} attempts!`);
