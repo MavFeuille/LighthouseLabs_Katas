2591a27cc15908fa171b7135fa9709b00d2e6886
MavFeuille <mavis.feuille@gmail.com>
2022-03-04 21:57:12 -0500

    Add if else statement for getting right randomNumber

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -11,9 +11,18 @@ const guessNumber = (randomNumber) => {
   if (prompt < randomNumber) {
     console.log("You answerd: " + input);
     console.log("Tow low!");
+    attempt += 1;
   } else if (prompt > randomNumber) {
     console.log("You answerd: " + input);
     console.log("Tow high!");
+    attempt += 1;
+  } else if (isNaN(prompt)) {
+    console.log("You answerd: " + input);
+    console.log("Not a number, try again!")
+  } else if (prompt === randomNumber) {
+    attempt += 1;
+    console.log("You answerd: " + input);
+    console.log(`You got it! You took ${attempt} attempts!`);
   }
 }
 
