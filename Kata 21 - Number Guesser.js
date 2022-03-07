041f22708c8ce5b433abeb8e3f8cc11d2fb33153
MavFeuille <mavis.feuille@gmail.com>
2022-03-06 23:52:03 -0500

    Remove while loop

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -9,23 +9,21 @@ const guessNumber = (randomNumber) => {
   let attempt = 0;
   let input = Number(prompt("Guess a number: "));
   
-  while (input !== randomNumber) {
-    if (input < randomNumber) {
-      console.log("You answerd: " + input);
-      console.log("Tow low!");
-      attempt += 1;
-    } else if (input > randomNumber) {
-      console.log("You answerd: " + input);
-      console.log("Tow high!");
-      attempt += 1;
-    } else if (typeof input !== Number) {
-      console.log("You answerd: " + input);
-      console.log("Not a number, try again!")
-    } else if (input == randomNumber) {
-      attempt += 1;
-      console.log("You answerd: " + input);
-      console.log(`You got it! You took ${attempt} attempts!`);
-    }
+  if (input < randomNumber) {
+    console.log("You answerd: " + input);
+    console.log("Tow low!");
+    attempt += 1;
+  } else if (input > randomNumber) {
+    console.log("You answerd: " + input);
+    console.log("Tow high!");
+    attempt += 1;
+  } else if (typeof input !== Number) {
+    console.log("You answerd: " + input);
+    console.log("Not a number, try again!")
+  } else if (input == randomNumber) {
+    attempt += 1;
+    console.log("You answerd: " + input);
+    console.log(`You got it! You took ${attempt} attempts!`);
   }
 }
 
