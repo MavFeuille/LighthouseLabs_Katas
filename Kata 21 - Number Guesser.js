785eebbb45ad49d8ff2fd7808f24a3d3fd1e8773
MavFeuille <mavis.feuille@gmail.com>
2022-03-06 23:48:28 -0500

    Create while loop to run guessing conditional statements

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -9,21 +9,23 @@ const guessNumber = (randomNumber) => {
   let attempt = 0;
   let input = Number(prompt("Guess a number: "));
   
-  if (input < randomNumber) {
-    console.log("You answerd: " + input);
-    console.log("Tow low!");
-    attempt += 1;
-  } else if (input > randomNumber) {
-    console.log("You answerd: " + input);
-    console.log("Tow high!");
-    attempt += 1;
-  } else if (typeof input !== Number) {
-    console.log("You answerd: " + input);
-    console.log("Not a number, try again!")
-  } else if (input == randomNumber) {
-    attempt += 1;
-    console.log("You answerd: " + input);
-    console.log(`You got it! You took ${attempt} attempts!`);
+  while (input !== randomNumber) {
+    if (input < randomNumber) {
+      console.log("You answerd: " + input);
+      console.log("Tow low!");
+      attempt += 1;
+    } else if (input > randomNumber) {
+      console.log("You answerd: " + input);
+      console.log("Tow high!");
+      attempt += 1;
+    } else if (typeof input !== Number) {
+      console.log("You answerd: " + input);
+      console.log("Not a number, try again!")
+    } else if (input == randomNumber) {
+      attempt += 1;
+      console.log("You answerd: " + input);
+      console.log(`You got it! You took ${attempt} attempts!`);
+    }
   }
 }
 
