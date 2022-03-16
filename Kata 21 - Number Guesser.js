f5ad417e55a47d7664025552b4c5faadb47a2d46
MavFeuille <mavis.feuille@gmail.com>
2022-03-15 23:02:09 -0400

    Set up guessArray.forEach to check if the user has input same guess before

--- Kata 21 - Number Guesser.js
+++ Kata 21 - Number Guesser.js
@@ -10,27 +10,36 @@ console.log("secretNumber = ",secretNumber)
 const guessNumber = (secretNumber) => {
   let attempt = 0;
   let guess;
-  
+  let guessArray = [];
+  let alreadyGuessed = [];
   // Prompt user to guess a number
   // Run through to check if guess === secretNumber
   // if guess !== secretNumber, increase count of attempt
   // Prompt user to guess again
   do {
     guess = Number(prompt("Guess a number: "));
-    
-    attempt ++;
-    if (guess < secretNumber) {
-      console.log("Too low!");
-    }
-    if (guess > secretNumber) {
-      console.log("Too high!")
-    }
-    if (isNaN(guess)) {
-      console.log("Not a number, Try again!")
-    }
-    if (guess === secretNumber) {
-      console.log(`You got it! You took ${attempt} attempts!`);
-    }
+    guessArray.push(guess);
+    console.log("guessArray at Line21 : ", guessArray);
+
+    guessArray.forEach((input) => {
+      if (alreadyGuessed[input]) {
+        console.log("Already guesed!");
+      } else {
+        attempt ++;
+        if (guess < secretNumber) {
+          console.log("Too low!");
+        }
+        if (guess > secretNumber) {
+          console.log("Too high!")
+        }
+        if (isNaN(guess)) {
+          console.log("Not a number, Try again!")
+        }
+        if (guess === secretNumber) {
+          console.log(`You got it! You took ${attempt} attempts!`);
+        }
+      }
+    })
   } while (guess !== secretNumber);
 }
 
